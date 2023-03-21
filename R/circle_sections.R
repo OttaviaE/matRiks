7b21ce78178cb1818f12274b36970fc090ce07dc
OttaviaE <otta.epifania@gmail.com>
2023-03-21 17:12:48 +0100

    Update circle sections

--- R/circle_sections.R
+++ R/circle_sections.R
@@ -1,22 +1,30 @@
-#' Define the coordinates of a pizza slice
+#' Coordinates of a pizza slice
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param theta1
-#' @param theta2
-#' @param lty
-#' @param lwd
-#' @param vis
-#' @param shd
+#' Define the coordinates of a circle section
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is 15.
+#' @param size.y The length of the x-axis. Default is 0.
+#' @param pos.x Position on the x axis. Default is 0.
+#' @param pos.y Position on the y axis, Default is 0.
+#' @param theta1 Starting angle of the circle section. Default is pi/4.
+#' @param theta2 Ending angle of the circle section (built counterclockwise). Default is 3*pi/4.
+#' @param lwd The line width. Default is 3.
+#' @param lty The lime type, default is 1 (solid line).
+#' @param vis Visibility of the figure. Default is 1, making the figure visible. To hide the figure, change it to 0.
+#' @param shd Color of the figure Default is NA which results in a transparent figure
+#' @return The coordinates of a circle section
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the pizza slice
+#' slice()
+#' # change the rotation of the pizza slice
+#' slice(theta1 = 3*pi/4, theta2 = 5*pi/4)
+#' }
 slice <- function(size.x =15,
-                  size.y = 0,pos.x=0,
+                  size.y = 0,
+                  pos.x=0,
                   pos.y=0,
                   theta1 = pi/4,
                   theta2 = 3*pi/4,
@@ -38,28 +46,36 @@ slice <- function(size.x =15,
     nv =  list(100),
     shade = list(shd),
     visible = vis,
-    tag = list("simple","fill", "rotate" )
+    tag = list("simple", "fill", "rotate", "d.int")
   )
   attr(value, "class") <- "figure"
   value
 }
-#' Define the coordinates of a pacman
+#' Coordinates of a pacman
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param theta1
-#' @param theta2
-#' @param lty
-#' @param lwd
-#' @param shd
-#' @param vis
+#' Define the coordinates of the circle sections drawing a pacman
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is sqrt(square()$ size.x[[1]]^2 /2).
+#' @param size.y The length of the x-axis. Default is 0.
+#' @param pos.x Position on the x axis. Default is 0.
+#' @param pos.y Position on the y axis, Default is 0.
+#' @param theta1 Starting angle of the circle section. Default is pi/4.
+#' @param theta2 Ending angle of the circle section. Default is 7*pi/4.
+#' @param lwd The line width. Default is 3.
+#' @param lty The lime type, default is 1 (solid line).
+#' @param vis Visibility of the figure. Default is 1, making the figure visible. To hide the figure, change it to 0.
+#' @param shd Color of the figure Default is NA which results in a transparent figure
+#'
+#' @return The coordinates of a pacman
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of pacman
+#' slice()
+#' # draw an actual pacman
+#' draw(cof(pacman(shd = "yellow"), dot(pos.y = 6)))
+#' }
 pacman <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
                    size.y = 0,pos.x=0 ,pos.y=0,
                    theta1 = pi/4,
@@ -81,42 +97,54 @@ pacman <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
     nv =  list(100),
     shade = list(shd),
     visible = vis,
-    tag = list("simple","fill", "rotate" )
+    tag = list("simple","fill", "rotate", "d.int" )
   )
   attr(value, "class") <- "figure"
   value
 }
-#' Define the coordinates of a pizza with four slices
+#' Coordinates of a pizza with four slices
 #'
-#' @param size.x
-#' @param shd
-#' @param lwd
-#' @param lty
+#' Define the coordinates of the circle section composing a pizza with four slices
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is 15.
+#' @param lwd The line width. Default is 3.
+#' @param lty The lime type, default is 1 (solid line).
+#' @param shd Color of the figure Default is NA which results in a transparent figure
+#'
+#' @return The coordinates of four circle sections composing a pizza with four slices
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the pizza with four slices
+#' pizza.4()
+#' }
 pizza.4 <- function(size.x = 15, shd = NA, lwd = 3, lty =1) {
   value <-cof(slice(size.x = size.x, shd = shd, lty = lty, lwd = lwd),
               slice(size.x = size.x,theta1 = 3*pi/4, theta2 = 5*pi/4, shd = shd,  lty = lty, lwd = lwd),
               slice(size.x = size.x,theta1 = 5*pi/4, theta2 = 7*pi/4, shd = shd, lty = lty, lwd = lwd),
               slice(size.x = size.x,theta1 = 7*pi/4, theta2 = 9*pi/4, shd = shd, lty = lty, lwd = lwd))
-  value$tag <- list("compose4","fill")
+  value$tag <- list("compose4","fill", "d.ext")
   attr(value, "class") <- "figure"
   value
 }
-#' Define the coordinates of a single pizza with four slices
+#' Coordinates of a single pizza with four slices
 #'
-#' @param size.x
-#' @param shd
-#' @param lwd
-#' @param lty
+#' Define the coordinates of the circle section composing a pizza with four slices, which is forced to be a single figure (to be used in shape())
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is 15.
+#' @param lwd The line width. Default is 3.
+#' @param lty The lime type, default is 1 (solid line).
+#' @param shd Color of the figure Default is NA which results in a transparent figure
+#'
+#' @return The coordinates of four circle sections composing a single pizza with four slices
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the single pizza with four slices
+#' s.pizza.4()
+#' }
 s.pizza.4 <- function(size.x = 15, shd = NA, lwd = 3, lty =1) {
   value <-cof(slice(size.x = size.x, shd = shd, lty = lty, lwd = lwd),
               slice(size.x = size.x,theta1 = 3*pi/4, theta2 = 5*pi/4, shd = shd,  lty = lty, lwd = lwd),
@@ -124,36 +152,42 @@ s.pizza.4 <- function(size.x = 15, shd = NA, lwd = 3, lty =1) {
               slice(size.x = size.x,theta1 = 7*pi/4, theta2 = 9*pi/4, shd = shd, lty = lty, lwd = lwd),
               name = "s.pizza.4",
               single = TRUE)
-  value$tag <- list("compose4","fill")
+  value$tag <- list("simple","fill", "d.int")
   attr(value, "class") <- "figure"
   value
 }
 
+#' Coordinates of a downward-facing left semi-circle
+#'
 #' Define the coordinates of a downward-facing left semi-circle
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param theta1
-#' @param theta2
-#' @param shd
-#' @param rotation
-#' @param lty
-#' @param lwd
-#' @param vis
+#' @param size.x The length of the x-axis. Default is sqrt(square()$ size.x[[1]]^2 /2).
+#' @param size.y The length of the x-axis. Default is 0.
+#' @param pos.x Position on the x axis. Default is 0.
+#' @param pos.y Position on the y axis, Default is 0.
+#' @param theta1 Starting angle of the circle section. Default is pi/4.
+#' @param theta2 Ending angle of the circle section (built counterclockwise). Default is 5*pi/4.
+#' @param shd Color of the figure Default is NA which results in a transparent figure.
+#' @param lwd The line width. Default is 3.
+#' @param lty The line type, default is 1 (solid line).
+#' @param vis Visibility of the figure. Default is 1, making the figure visible. To hide the figure, change it to 0.
 #'
-#' @return
+#' @return The coordinates a downward-facing left semi-circle
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the downward-facing left semi-circle
+#' semi.circle()
+#' # change the rotation of the downward-facing left semi-circle
+#' semi.circle(theta1 = pi/2, theta2 = 3*pi/2)
+#' }
 semi.circle <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
                         size.y = 0,
                         pos.x=0 ,pos.y=0,
                         theta1 = pi/4,
                         theta2 = 5*pi/4,
                         shd = NA,
-                        rotation = pi - pi / 4,
                         lty = 1,
                         lwd =3,
                         vis = 1) {
@@ -163,7 +197,7 @@ semi.circle <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
     size.y = list(size.y),
     theta.1  = list(theta1),
     theta.2  = list(theta2),
-    rotation = list(rotation),
+    rotation = list(0),
     pos.x = list(pos.x),
     pos.y = list(pos.y),
     lty = list(lty),
@@ -178,31 +212,37 @@ semi.circle <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
   value
 }
 
-#' Define the coordinates of an upward-facing right semi-circle
+#' Coordinates of a upward-facing left semi-circle
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param theta1
-#' @param theta2
-#' @param shd
-#' @param rotation
-#' @param lty
-#' @param lwd
-#' @param vis
+#' Define the coordinates of a upward-facing left semi-circle
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is sqrt(square()$ size.x[[1]]^2 /2).
+#' @param size.y The length of the x-axis. Default is 0.
+#' @param pos.x Position on the x axis. Default is 0.
+#' @param pos.y Position on the y axis, Default is 0.
+#' @param theta1 Starting angle of the circle section. Default is 5*pi/4.
+#' @param theta2 Ending angle of the circle section (built counterclockwise). Default is pi/4.
+#' @param shd Color of the figure Default is NA which results in a transparent figure.
+#' @param lwd The line width. Default is 3.
+#' @param lty The line type, default is 1 (solid line).
+#' @param vis Visibility of the figure. Default is 1, making the figure visible. To hide the figure, change it to 0.
+#'
+#' @return The coordinates a upward-facing left semi-circle
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the upward-facing left semi-circle
+#' semi.circle.inv()
+#' # change the rotation of the upward-facing left semi-circle
+#' semi.circle.inv(theta1 = pi, theta2 = 2*pi)
+#' }
 semi.circle.inv <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
                         size.y = 0,
                         pos.x=0 ,pos.y=0,
                         theta1 = 5*pi/4,
                         theta2 = pi/4,
                         shd = NA,
-                        rotation = pi - pi / 4,
                         lty = 1,
                         lwd =3,
                         vis = 1) {
@@ -212,7 +252,7 @@ semi.circle.inv <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
     size.y = list(size.y),
     theta.1  = list(theta1),
     theta.2  = list(theta2),
-    rotation = list(rotation),
+    rotation = list(0),
     pos.x = list(pos.x),
     pos.y = list(pos.y),
     lty = list(lty),
@@ -227,20 +267,23 @@ semi.circle.inv <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
   value
 }
 
-#' Define the coordinates of a pizza with two slices
+#' Coordinates of a pizza with two slices
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param shd
-#' @param lty
-#' @param lwd
+#' Define the coordinates of the circle section composing a pizza with two slices
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is 15.
+#' @param lwd The line width. Default is 3.
+#' @param lty The lime type, default is 1 (solid line).
+#' @param shd Color of the figure Default is NA which results in a transparent figure
+#'
+#' @return The coordinates of two circle sections composing a pizza with two slices
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the pizza with two slices
+#' pizza.2()
+#' }
 pizza.2 <- function(size.x = 15, size.y = 0,
                   pos.x=0 ,pos.y=0,
                   shd = NA, lty = 1, lwd = 3) {
@@ -262,20 +305,23 @@ pizza.2 <- function(size.x = 15, size.y = 0,
   attr(value, "class") <- "figure"
   value
 }
-#' Define the coordinates of a single pizza with two slices (to be used in diff_shapes)
+#' Coordinates of a single pizza with two slices
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param shd
-#' @param lty
-#' @param lwd
+#' Define the coordinates of the circle section composing a pizza with two slices, which is forced to be a single figure (to be used in shape())
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is 15.
+#' @param lwd The line width. Default is 3.
+#' @param lty The lime type, default is 1 (solid line).
+#' @param shd Color of the figure Default is NA which results in a transparent figure
+#'
+#' @return The coordinates of two circle sections composing a single pizza with two slices
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the single pizza with two slices
+#' s.pizza.2()
+#' }
 s.pizza.2 <- function(size.x = 15, size.y = 0,
                   pos.x=0 ,pos.y=0,
                   shd = NA, lty = 1, lwd = 3) {
@@ -299,24 +345,31 @@ s.pizza.2 <- function(size.x = 15, size.y = 0,
   attr(value, "class") <- "figure"
   value
 }
-#' Define the coordinates of an downward facing right semi-circle
+#' Coordinates of a downward-facing right semi-circle
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param theta1
-#' @param theta2
-#' @param shd
-#' @param rotation
-#' @param lty
-#' @param lwd
-#' @param vis
+#' Define the coordinates of a downward-facing right semi-circle
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is sqrt(square()$ size.x[[1]]^2 /2).
+#' @param size.y The length of the x-axis. Default is 0.
+#' @param pos.x Position on the x axis. Default is 0.
+#' @param pos.y Position on the y axis, Default is 0.
+#' @param theta1 Starting angle of the circle section. Default is 7*pi/4.
+#' @param theta2 Ending angle of the circle section (built counterclockwise). Default is 3*pi/4.
+#' @param shd Color of the figure Default is NA which results in a transparent figure.
+#' @param lwd The line width. Default is 3.
+#' @param lty The line type, default is 1 (solid line).
+#' @param vis Visibility of the figure. Default is 1, making the figure visible. To hide the figure, change it to 0.
+#'
+#' @return The coordinates a downward-facing right semi-circle
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the downward-facing right semi-circle
+#' semi.circle.rev()
+#' # change the rotation of the downward-facing right semi-circle
+#' semi.circle.rev(theta1 = 0, theta2 = pi/2)
+#' }
 semi.circle.rev <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
                         size.y = 0,
                         pos.x=0 ,pos.y=0,
@@ -347,24 +400,31 @@ semi.circle.rev <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
   attr(value, "class") <- "figure"
   value
 }
-#' Define the coordinates of an upward facing left semi-circle
+#' Coordinates of a upward-facing left semi-circle
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param theta1
-#' @param theta2
-#' @param shd
-#' @param rotation
-#' @param lty
-#' @param lwd
-#' @param vis
+#' Define the coordinates of a upward-facing left semi-circle
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is sqrt(square()$ size.x[[1]]^2 /2).
+#' @param size.y The length of the x-axis. Default is 0.
+#' @param pos.x Position on the x axis. Default is 0.
+#' @param pos.y Position on the y axis, Default is 0.
+#' @param theta1 Starting angle of the circle section. Default is 3*pi/4.
+#' @param theta2 Ending angle of the circle section (built counterclockwise). Default is 7*pi/4.
+#' @param shd Color of the figure Default is NA which results in a transparent figure.
+#' @param lwd The line width. Default is 3.
+#' @param lty The line type, default is 1 (solid line).
+#' @param vis Visibility of the figure. Default is 1, making the figure visible. To hide the figure, change it to 0.
+#'
+#' @return The coordinates a upward-facing left semi-circle
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the upward-facing left semi-circle
+#' semi.circle.rev.inv()
+#' # change the rotation of the upward-facing left semi-circle
+#' semi.circle.rev.inv(theta1 = pi, theta2 = 2*pi)
+#' }
 semi.circle.rev.inv <- function(size.x =sqrt(square()$ size.x[[1]]^2 /2),
                             size.y = 0,
                             pos.x=0 ,pos.y=0,
@@ -430,20 +490,23 @@ pizza.2.rev <- function(size.x = 15, size.y = 0,
   attr(value, "class") <- "figure"
   value
 }
-#' Define the coordinates of a single inverse pizza with two slices (to be used in diff_shapes)
+#' Coordinates of a single inverse pizza with two slices
 #'
-#' @param size.x
-#' @param size.y
-#' @param pos.x
-#' @param pos.y
-#' @param shd
-#' @param lty
-#' @param lwd
+#' Define the coordinates of the circle section composing an inverse pizza with two slices, which is forced to be a single figure (to be used in shape())
 #'
-#' @return
+#' @param size.x The length of the x-axis. Default is 15.
+#' @param lwd The line width. Default is 3.
+#' @param lty The lime type, default is 1 (solid line).
+#' @param shd Color of the figure Default is NA which results in a transparent figure
+#'
+#' @return The coordinates of two circle sections composing a single pizza with two slices
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#' # default coordinates of the single inverse pizza with two slices
+#' s.pizza.2()
+#' }
 s.pizza.2.rev <- function(size.x = 15, size.y = 0,
                     pos.x=0 ,pos.y=0,
                     shd = NA, lty = 1, lwd = 3) {
