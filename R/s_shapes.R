b18ef9e3cae0948ce72337a8480b02f67581d257
OttaviaE <otta.epifania@gmail.com>
2023-05-08 15:01:36 +0200

    Update code I

--- R/s_shapes.R
+++ R/s_shapes.R
@@ -1,13 +1,21 @@
-#'  Define the cooordinates for a vertical s
+#' Coordinates of a vertical S
 #'
-#' @param lwd
-#' @param lty
+#' Define the coordinates of a vertical S
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing a vertical S
 #' @export
 #'
-#' @examples Arrivano
-s.vertical <- function(lty= 1,
+#' @examples
+#' \dontrun{
+#' # default coordinates of the vertical S
+#' vertical.s()
+#' # change the line type
+#' vertical.s(lty = 2)
+#' }
+vertical.s <- function(lty= 1,
                        lwd = 3) {
   value <-cof( v.arc.left.up(lty = lty, lwd = lwd),
                v.arc.right.down(lty = lty, lwd = lwd))
@@ -15,16 +23,24 @@ s.vertical <- function(lty= 1,
   attr(value, "class") <- "figure"
   value
 }
-#'  Define the cooordinates for a vertical inverted s
+#' Coordinates of an inverted vertical S
 #'
-#' @param lty
-#' @param lwd
+#' Define the coordinates of an inverted vertical S
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing an inverted vertical S
 #' @export
 #'
 #' @examples
-s.vertical.inv <- function(lty =1,
+#' \dontrun{
+#' # default coordinates of the inverted vertical S
+#' vertical.s.inv()
+#' # change the line type
+#' vertical.s.inv(lty = 2)
+#' }
+vertical.s.inv <- function(lty =1,
                            lwd = 3) {
   value <-cof( v.arc.right.up(lty = lty, lwd = lwd),
                v.arc.left.down(lty = lty, lwd = lwd))
@@ -32,104 +48,152 @@ s.vertical.inv <- function(lty =1,
   attr(value, "class") <- "figure"
   value
 }
-#'  Define the cooordinates for an horizontal s
+#' Coordinates of an horizontal S
 #'
-#' @param lty
-#' @param lwd
+#' Define the coordinates of an horizontal S
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing an horizontal S
 #' @export
 #'
 #' @examples
-s.horizontal <- function(lty = 1, lwd = 3) {
+#' \dontrun{
+#' # default coordinates of the horizontal S
+#' horizontal.s()
+#' # change the line type
+#' horizontal.s(lty = 2)
+#' }
+horizontal.s <- function(lty = 1, lwd = 3) {
   value <-cof( h.arc.left.up(lty = lty, lwd = lwd), h.arc.right.down(lty = lty, lwd = lwd))
   value$tag <- list("compose2","fill")
   attr(value, "class") <- "figure"
   value
 }
-#'  Define the cooordinates for an horizontal invertred s
+#' Coordinates of an inverted horizontal S
 #'
-#' @param lty
-#' @param lwd
+#' Define the coordinates of an inverted horizontal S
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing an horizontal S
 #' @export
 #'
 #' @examples
-s.horizontal.inv <- function(lty = 1, lwd = 3) {
+#' \dontrun{
+#' # default coordinates of the horizontal S
+#' horizontal.s.inv()
+#' # change the line type
+#' horizontal.s.inv(lty = 2)
+#' }
+horizontal.s.inv <- function(lty = 1, lwd = 3) {
   value <-cof( h.arc.left.down(lty = lty, lwd = lwd),
                h.arc.right.up(lty = lty, lwd = lwd))
   value$tag <- list("compose2","fill")
   attr(value, "class") <- "figure"
   value
 }
-#'  Define the cooordinates for a single vertical s (to be used in diff_shapes)
+#' Coordinates of a single vertical S
 #'
-#' @param lty
-#' @param lwd
+#' Define the coordinates for drawing a single vertical S composed of two arches, which is forced to be a single figure (to be used in shape())
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing a vertical S
 #' @export
 #'
 #' @examples
-s.s.vertical <- function(lty= 1,
+#' \dontrun{
+#' # default coordinates of the vertical S
+#' s.vertical.s()
+#' # change the line type
+#' s.vertical.s(lty = 2)
+#' }
+s.vertical.s <- function(lty= 1,
                          lwd = 3) {
   value <-cof( v.arc.left.up(lty = lty, lwd = lwd), v.arc.right.down(lty = lty, lwd = lwd),
-               single=TRUE, name="s.vertical")
+               single=TRUE, name="vertical.s")
   value$tag <- list("simple","fill")
   attr(value, "class") <- "figure"
   value
 }
-#'  Define the cooordinates for a single inverted vertical s (to be used in diff_shapes)
+#' Coordinates of a single inverted vertical S
 #'
-#' @param lty
-#' @param lwd
+#' Define the coordinates for drawing a single inverted vertical S composed of two arches, which is forced to be a single figure (to be used in shape())
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing a single vertical S
 #' @export
 #'
 #' @examples
-s.s.vertical.inv <- function(lty= 1,
+#' \dontrun{
+#' # default coordinates of the single inverted vertical S
+#' s.vertical.s.inv()
+#' # change the line type
+#' s.vertical.s.inv(lty = 2)
+#' }
+s.vertical.s.inv <- function(lty= 1,
                              lwd = 3) {
   value <-cof( v.arc.right.up(lty = lty, lwd = lwd), v.arc.left.down(lty = lty, lwd = lwd),single=TRUE,
-               name="s.vertical.inv")
+               name="vertical.s.inv")
   value$tag <- list("simple","fill")
   attr(value, "class") <- "figure"
   value
 }
-#'  Define the cooordinates for a single horizontal s (to be used in diff shapes)
+#' Coordinates of a single horizontal S
 #'
-#' @param lty
-#' @param lwd
+#' Define the coordinates for drawing a single horizontal S composed of two arches, which is forced to be a single figure (to be used in shape())
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing a single horizontal S
 #' @export
 #'
 #' @examples
-s.s.horizontal <- function(lty= 1,
+#' \dontrun{
+#' # default coordinates of the single horizontal S
+#' s.horizontal.s()
+#' # change the line type
+#' s.horizontal.s(lty = 2)
+#' }
+s.horizontal.s <- function(lty= 1,
                            lwd = 3) {
   value <-cof(h.arc.left.up(lty = lty, lwd = lwd),
               h.arc.right.down(lty = lty, lwd = lwd),
-              name="s.horizontal",
+              name="horizontal.s",
               single=TRUE)
   value$tag <- list("simple","fill")
   attr(value, "class") <- "figure"
   value
 }
-#'  Define the cooordinates for a single inverted horizontal s (to be used in diff shapes)
+#' Coordinates of a single inverted horizontal S
 #'
-#' @param lty
-#' @param lwd
+#' Define the coordinates for drawing a single inverted horizontal S composed of two arches, which is forced to be a single figure (to be used in shape())
 #'
-#' @return
+#' @param lwd integer, define the line width of the figure. Default is 3.
+#' @param lty integer, define the line type of the figure, default is 1 (solid line).
+#'
+#' @return Return the coordinates for drawing a single inverted horizontal S
 #' @export
 #'
 #' @examples
-s.s.horizontal.inv <- function(lty= 1,
+#' \dontrun{
+#' # default coordinates of the single inverted horizontal S
+#' s.horizontal.s.inv()
+#' # change the line type
+#' s.horizontal.s.inv(lty = 2)
+#' }
+s.horizontal.s.inv <- function(lty= 1,
                                lwd = 3) {
   value <-cof( h.arc.left.down(lty = lty,  lwd = lwd),
                h.arc.right.up(lty = lty,  lwd = lwd),single=TRUE,
-               name="s.s.horizontal.inv")
+               name="s.horizontal.s.inv")
   value$tag <- list("simple","fill")
   attr(value, "class") <- "figure"
   value
