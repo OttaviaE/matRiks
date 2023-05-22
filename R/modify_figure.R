0e253fe7a9a9045caa2f4334e8c7bf9ac31ab1dc
andrea <andreabrancaccio01@gmail.com>
2023-05-22 17:51:06 +0200

    Modified figure esportati

--- R/modify_figure.R
+++ R/modify_figure.R
@@ -7,6 +7,7 @@
 #' @param index The index of the element to hide
 #'
 #' @return An object with changed visibility
+#' @export hide
 #' @export
 #'
 #' @examples
@@ -21,6 +22,28 @@
 hide<- function(obj,index) {
   UseMethod("hide")
 }
+
+#' Hide figures
+#'
+#' Change the visibility of the single objects composing a figure from 1 to 0
+#'
+#'
+#' @param obj The figure of a matrix
+#' @param index The index of the element to hide
+#'
+#' @return An object with changed visibility
+#' @export hide.figure
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # concanate three figures into an object
+#' my_shapes = cof(square(), triangle(), slice())
+#' # draw object
+#' draw(my_shapes)
+#' # show the triangle
+#' draw(show(my_shapes, 2))
+#' }
 hide.figure<-function(obj,index="Full")
 {
   if(any(index=="Full"))
@@ -39,6 +62,7 @@ hide.figure<-function(obj,index="Full")
 #' @param index The index of the element to show
 #'
 #' @return An object with changed visibility
+#' @export show
 #' @export
 #'
 #' @examples
@@ -53,7 +77,26 @@ hide.figure<-function(obj,index="Full")
 show<- function(obj,index) {
   UseMethod("show")
 }
-
+#' Show figures
+#'
+#' Change the visibility of the single objects composing a figure from 0 to 1
+#'
+#' @param obj The figure of a matrix
+#' @param index The index of the element to show
+#'
+#' @return An object with changed visibility
+#' @export show.figure
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # concanate three figures into an object
+#' my_shapes = cof(square(), triangle(), slice())
+#' # draw object
+#' draw(my_shapes)
+#' # show the triangle
+#' draw(show(my_shapes, 2))
+#' }
 show.figure<-function(obj,index="Full")
 {
   if(any(index=="Full"))
@@ -74,6 +117,7 @@ show.figure<-function(obj,index="Full")
 #' @return An object with a changed figure
 #'
 #' @return Return an object with a different figure
+#' @export replace
 #' @export
 #'
 #' @examples
@@ -88,6 +132,29 @@ show.figure<-function(obj,index="Full")
 replace <- function(obj,index,replacement) {
   UseMethod("replace")
 }
+#' Replace objects
+#'
+#' Replace an object in a figure with another object
+#'
+#' @param obj The figure of a matrix
+#' @param index The index of the element to replace
+#' @param replacement The name of the figure with which the original one is replaced
+#'
+#' @return An object with a changed figure
+#'
+#' @return Return an object with a different figure
+#' @export replace.figure
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # concanate three figures into an object
+#' my_shapes = cof(square(), triangle(), slice())
+#' # draw object
+#' draw(my_shapes)
+#' # replace the square with a gray pacman
+#' draw(replace(my_shapes, 1, pacman(shd = "grey")))
+#' }
 
 replace.figure<-function(obj,index,replacement)
 {
