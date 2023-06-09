569a85d11aa75afed90c7f2ffd7bd25ea39ab026
OttaviaE <otta.epifania@gmail.com>
2023-06-09 12:15:16 +0200

    ic flip

---
+++ R/ic_flip.R
@@ -0,0 +1,39 @@
+#' Flip incomplete correlate (method)
+#'
+#' @param obj Matriks
+#'
+#' @return A list
+#' @export repetition
+#' @export
+#'
+#' @examples
+ic_flip <- function(obj) {
+  UseMethod("ic_flip")
+}
+
+#' flipative incomplete correlate
+#'
+#' Change color to the correct response for creating the ic flip distractors
+#'
+#' @param obj The matriks
+#' @param ... Other arguments
+#'
+#' @return A list
+#' @export ic_flip.matriks
+#' @export
+#'
+#' @examples
+ic_flip.matriks <- function(obj, ...) {
+  m_correct <- correct(obj)
+  if (class(obj$mat.type) == "numeric") {
+    dist_ic_flip <- change_color(m_correct)
+  } else {
+    split_correct <- split_mat(obj)
+    dist_ic_flip <-  replace(m_correct, length(m_correct$shape),
+                            rotate(split_correct[[length(split_correct)]]))
+
+  }
+
+  class(dist_ic_flip) <- "figure"
+  return(dist_ic_flip)
+}
