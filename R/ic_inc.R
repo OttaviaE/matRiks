3067f074b635c2fa3a24ec95a35118b254a919c0
OttaviaE <otta.epifania@gmail.com>
2023-06-09 12:34:19 +0200

    IC inc

---
+++ R/ic_inc.R
@@ -0,0 +1,38 @@
+#' Incomplete incomplete correlate (method)
+#'
+#' @param obj Matriks
+#'
+#' @return A list
+#' @export ic_inc
+#' @export
+#'
+#' @examples
+ic_inc <- function(obj) {
+  UseMethod("ic_inc")
+}
+
+#' incative incomplete correlate
+#'
+#' Change color to the correct response for creating the ic inc distractors
+#'
+#' @param obj The matriks
+#' @param ... Other arguments
+#'
+#' @return A list
+#' @export ic_inc.matriks
+#' @export
+#'
+#' @examples
+ic_inc.matriks <- function(obj, ...) {
+  m_correct <- correct(obj)
+  if (class(obj$mat.type) == "numeric") {
+    dist_ic_inc <- change_color(m_correct)
+  } else {
+    split_correct <- split_mat(obj)
+    dist_ic_inc <-  hide(m_correct, length(m_correct$shape))
+
+  }
+
+  class(dist_ic_inc) <- "figure"
+  return(dist_ic_inc)
+}
