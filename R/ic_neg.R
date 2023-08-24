c4c3c5f98f20472759730c54d8151dea4cdb4881
OttaviaE <otta.epifania@gmail.com>
2023-08-24 11:37:36 +0200

    ic neg

--- R/ic_neg.R
+++ R/ic_neg.R
@@ -43,15 +43,27 @@ ic_neg <- function(obj, ...) {
 #' draw(ic_neg(m1))
 #' }
 ic_neg.matriks <- function(obj, ...) {
+  w <- FALSE
   m_correct <- correct(obj)
   if (inherits(obj$mat.type, "numeric") == TRUE) {
     if (all(!unlist(m_correct$tag) == "fill")) {
-      dist_ic_neg <- margin(m_correct, 2, "lty")
+      w <- TRUE
+      dist_ic_neg <- cof(m_correct, size(X(lwd = 10), 3, "inv"))
+      warning("Can't change color, sorry!")
     } else {
       dist_ic_neg <- change_color(m_correct)
     }
   } else {
-    split_correct <- decof(m_correct)
+    # split_correct <- decof(m_correct)
+    # if (any(m_correct$tag[[length(m_correct$tag)]] == "compose4" | m_correct$tag[[length(m_correct$tag)]] == "compose2")) {
+    #   index <- unlist(lapply(m_correct$tag,function(x){as.integer(gsub("compose", "",  x[grepl("compose", x)]))}))
+    #   changing<-(length(split_correct)-index[length(index)]+1):length(split_correct)
+    #   transvestite <- which(m_correct$visible==1)
+    #   transvestite <- intersect((length(m_correct$visible)-index[length(index)]+1):(max(transvestite)),transvestite)
+    #   trans<-transvestite-(max(transvestite)-max(changing))
+    #   changing<-intersect(trans,changing)
+    split_correct <- split_mat(obj)
+
     if (any(m_correct$tag[[length(m_correct$tag)]] == "compose4" | m_correct$tag[[length(m_correct$tag)]] == "compose2")) {
       index <- unlist(lapply(m_correct$tag,function(x){as.integer(gsub("compose", "",  x[grepl("compose", x)]))}))
       changing<-(length(split_correct)-index[length(index)]+1):length(split_correct)
@@ -60,14 +72,19 @@ ic_neg.matriks <- function(obj, ...) {
       trans<-transvestite-(max(transvestite)-max(changing))
       changing<-intersect(trans,changing)
 
-
+#
+#       new_image<-list()
+#       for (i in 1:length(changing)) {
+#         if (all(!unlist(split_correct[[changing[i]]]) == "fill",na.rm=TRUE)) {
+#           new_image[[i]] <-  margin(split_correct[[changing[i]]], 2, "lty")
+#         } else {
+#           new_image[[i]] <-  change_color(split_correct[[changing[i]]])
+#         }
+#       }
       new_image<-list()
       for (i in 1:length(changing)) {
-        if (all(!unlist(split_correct[[changing[i]]]) == "fill",na.rm=TRUE)) {
-          new_image[[i]] <-  margin(split_correct[[changing[i]]], 2, "lty")
-        } else {
-          new_image[[i]] <-  change_color(split_correct[[changing[i]]])
-        }
+        new_image[[i]] <-  change_color(split_correct[[changing[i]]])
+
       }
 
 
@@ -76,20 +93,26 @@ ic_neg.matriks <- function(obj, ...) {
       changing <- sum(m_correct$visible)
       transvestite<-which(m_correct$visible==1)
       if (all(!unlist(split_correct[[changing]]$tag) == "fill",na.rm=TRUE)) {
-        new_image <- cof(m_correct, size(X(lwd = 10), 3, "inv"))
+        w <- TRUE
         warning("Can't change color, sorry!")
       } else {
       new_image <- list(change_color(split_correct[[changing]]))
     }
 }
-    dist_ic_neg <-m_correct
-    transvestite<-which(m_correct$visible==1)
-    for(i in 1:length(changing)){
-      dist_ic_neg <-  replace(dist_ic_neg,
-                              #intersect(transvestite, changing[i]),
-                              transvestite[changing[i]],
-                              new_image[[i]])
+
+    if (w == TRUE) {
+      dist_ic_neg <- cof(m_correct, size(X(lwd = 10), 3, "inv"))
+    } else {
+      dist_ic_neg <-m_correct
+      for(i in 1:length(changing)){
+        transvestite<-which(m_correct$visible==1)
+        dist_ic_neg <-  replace(dist_ic_neg,
+                                #intersect(transvestite, changing[i]),
+                                transvestite[changing[i]],
+                                new_image[[i]])
+      }
     }
+
 
   }
 
