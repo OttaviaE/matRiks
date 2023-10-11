5fa13c7e5ec16c26259a5c34e6728a76616e883e
andrea <andreabrancaccio01@gmail.com>
2023-10-11 19:38:29 +0200

    Test dritti e gaii per margin e shade

--- tests/testthat/test-rules.R
+++ tests/testthat/test-rules.R
@@ -45,7 +45,7 @@ test_that("Size doesn't matter! Decreasing size", {
   }
 })
 
-test_that("Change shape straigth", {
+test_that("Change shape straight", {
   for(n in 1:9)
   {
     fig1 <- cof(square(), square(),s_lily())
@@ -62,5 +62,48 @@ test_that("Change shape reverse", {
     fig2 <- shape(fig1, n = n ,rule = "shape.inv")
     index<-c(3:1,3:1,3:1) #TR-LL
     expect_equal(which(fig2$visible==1), index[n])
+  }
+})
+
+test_that("Change shade straight", {
+  for(n in 1:9)
+  {
+    fig1 <- cof(square())
+    fig2 <- shade(fig1, n = n ,rule = "shade")
+    index <- rep(c("white","grey","black"),3)
+
+    position<-unlist(lapply(fig1$shade, function(x,i,n)
+    {
+      lx <- length(x)
+      pos <- index==x[1]
+      if(is.na(sum(pos)))
+      {
+        return(n)
+      }else{
+        pos <- which(pos)
+        return(rep(pos[1]+n,lx))
+      }
+    },index,n))
+    expect_equal(unlist(fig2$shade), index[position])
+  }
+})
+
+test_that("Change margin width straight", {
+  for(n in 1:3)
+  {
+    fig1 <- cof(square())
+    fig2 <- margin(fig1, n = n ,rule = "margin.lwd")
+    index<-c(1:3,1:3,1:3) #TR-LL
+    expect_equal(fig2$lwd, lapply(fig1$lwd,function(x,i,n){i[x+n]+1},index,n))
+  }
+})
+
+test_that("Change margin  width reverse", {
+  for(n in 1:3)
+  {
+    fig1 <- cof(circle())
+    fig2 <- margin(fig1, n = n ,rule = "margin.lwd.inv")
+    index<-c(3:1,3:1,3:1) #TR-LL
+    expect_equal(fig2$lwd, lapply(fig1$lwd,function(x,i,n){i[x+n]+1},index,n))
   }
 })
