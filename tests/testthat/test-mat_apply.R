3e28d98ca212d2b0cfec5766a318d7ca9de233a2
Andrea Brancaccio <andreabrancaccio01@gmail.com>
2023-10-12 09:33:09 +0100

    mattapply

--- tests/testthat/test-mat_apply.R
+++ tests/testthat/test-mat_apply.R
@@ -1,3 +1,10 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("matrix generator error test", {
+  fig1 <- circle()
+  fig2 <- cof(circle(),square(),s_lily())
+  expect_warning(mat_apply(fig1,hrules=c("identity","identity")))
+  # Logical rules on a 2x2 matrix
+  expect_error(mat_apply(fig1,hrules=c("AND"),mat.type = 4))
+  # Logical and visuo-spatial rules on the same layer
+  expect_error(mat_apply(fig2,hrules=c("AND","shapes"),mat.type = 9))
 })
+
